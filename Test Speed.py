83e6530dcd57ca758b326d568692be23098640d0
Tobyisepik <116378333+Tobyisepik@users.noreply.github.com>
2023-08-13 20:55:09 +0100

    testingSpeeds

--- Test Speed.py
+++ Test Speed.py
@@ -1,9 +1,23 @@
 import cv2
-
+import time
+import os
 
 cam=cv2.VideoCapture(r"Video/Video.mp4")
-
+totalDiff=0
+amount=0
 while True:
+    
+    start=time.time()
     ret,img=cam.read()
     cv2.imshow("",img)
     cv2.waitKey(1)
+    end=time.time()
+    diff=end-start
+    diff*=10
+    totalDiff+=diff
+    totalDiff*10
+    amount+=1
+    
+    avgTime=totalDiff/amount
+    os.system("cls")
+    print("Average processing time is:",avgTime/10,"seconds")
